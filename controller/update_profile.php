66a2fb2e7f5a7fede64d5aaed2a77bf08d1aa985
onostar <onostarkels@gmail.com>
2023-05-26 15:37:42 +0100

    update profile with state code

--- controller/update_profile.php
+++ controller/update_profile.php
@@ -20,28 +20,17 @@
         $user_type  = "Delegate";
         $country  = "Nigeria";
         $group = htmlspecialchars(stripslashes($_POST['tech_group']));
-        $int_group = htmlspecialchars(stripslashes($_POST['interest_group']));
+        // $int_group = htmlspecialchars(stripslashes($_POST['interest_group']));
         $passport = $_FILES['passport']['name'];
         $passport_folder = "../passports/".$passport;
         $id = $_POST['user_id'];
-        $passport_size = $_FILES['company_logo']['size'];
+        $passport_size = $_FILES['passport']['size'];
         $allowed_ext = array('png', 'jpg', 'jpeg', 'webp');
         /* get current file extention */
         $file_ext = explode('.', $passport);
         $file_ext = strtolower(end($file_ext));
 
-        /* get state_id */
-        $get_state_num = $connectdb->prepare('SELECT * FROM users WHERE res_state = :res_state');
-        $get_state_num->bindvalue("res_state", $state);
-        $get_state_num->execute();
-        if($get_state_num->rowCount() > 0){
-            $state_id = $get_state_num->rowCount();
-        }else{
-            $state_id = 1;
-        }
-        $code = strtoupper(substr($state, 0, 3));
-        $cur_time = Date("Y");
-        $reg_number = $code."/".$cur_time ."/0".$state_id."/0".$id;
+        
         /* get barcode details and create */
         $barcode_num = random_int(100000000, 200000000).$id;
         
@@ -55,12 +44,12 @@
 
         // check image type and size before updating profile
         if(in_array($file_ext, $allowed_ext)){
-            if($logo_size <= 300000){
+            if($passport_size <= 300000){
                 /* update profile */
                 if(move_uploaded_file($_FILES['passport']['tmp_name'], $passport_folder)){
                     
                     // update values
-                    $update_profile = $connectdb->prepare("UPDATE users SET first_name = :first_name, last_name = :last_name, whatsapp = :whatsapp, other_number = :other_number, res_state = :res_state, user_email = :user_email, passport = :passport, reg_number = :reg_number, gender = :gender, tech_group = :tech_group, interest_group = :interest_group, user_type = :user_type, country = :country, barcode = :barcode WHERE user_id = :user_id");
+                    $update_profile = $connectdb->prepare("UPDATE users SET first_name = :first_name, last_name = :last_name, whatsapp = :whatsapp, other_number = :other_number, res_state = :res_state, user_email = :user_email, passport = :passport, gender = :gender, tech_group = :tech_group, user_type = :user_type, country = :country, barcode = :barcode WHERE user_id = :user_id");
                     $update_profile->bindvalue("first_name", $first_name);
                     $update_profile->bindvalue("last_name", $last_name);
                     $update_profile->bindvalue("whatsapp", $whatsapp);
@@ -68,10 +57,10 @@
                     $update_profile->bindvalue("res_state", $state);
                     $update_profile->bindvalue("user_email", $email);
                     $update_profile->bindvalue("passport", $passport);
-                    $update_profile->bindvalue("reg_number", $reg_number);
+                    // $update_profile->bindvalue("reg_number", $reg_number);
                     $update_profile->bindvalue("gender", $gender);
                     $update_profile->bindvalue("tech_group", $group);
-                    $update_profile->bindvalue("interest_group", $int_group);
+                    // $update_profile->bindvalue("interest_group", $int_group);
                     $update_profile->bindvalue("user_type", $user_type);
                     $update_profile->bindvalue("country", $country);
                     $update_profile->bindvalue("barcode", $barcode_num);
@@ -80,9 +69,31 @@
                     $update_profile->execute();
 
                     if($update_profile){
-                        $_SESSION['success'] = "Profile updated successfully!";
-                        header("Location: ../views/user.php");
-                        // echo "<img alt='testing' src='barcode/barcode.php?codetype=Code39&size=40&text=".$barcode_num."&print=true'/>";
+                        /* get state_id */
+                        $get_state_num = $connectdb->prepare('SELECT * FROM users WHERE res_state = :res_state');
+                        $get_state_num->bindvalue("res_state", $state);
+                        $get_state_num->execute();
+                        if($get_state_num->rowCount() > 0){
+                            $state_id = $get_state_num->rowCount();
+                        }else{
+                            $state_id = 1;
+                        }
+                        $code = strtoupper(substr($state, 0, 3));
+                        $cur_time = Date("Y");
+                        $reg_number = $code."/".$cur_time ."/0".$state_id."/0".$id;
+                        // update reg number with state
+                        $update_reg = $connectdb->prepare("UPDATE users SET reg_number = :reg_number WHERE user_id = :user_id");
+                        $update_reg->bindValue("reg_number", $reg_number);
+                        $update_reg->bindValue("user_id", $id);
+                        $update_reg->execute();
+                        if($update_reg){
+                            $_SESSION['success'] = "Profile updated successfully!";
+                            header("Location: ../views/user.php");
+                        }else{
+                            $_SESSION['error'] = "Update registration failed";
+                            header("Location: ../views/welcome_page.php");
+                        }
+                        
                     }else{
                         $_SESSION['error'] = "Update failed";
                         header("Location: ../views/welcome_page.php");
