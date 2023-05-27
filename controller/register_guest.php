8cbc312aff9a37b7e3f44e61a653a4afdfc9691a
onostar <onostarkels@gmail.com>
2023-05-27 13:49:15 +0100

    updataed passport size on guest registration

--- controller/register_guest.php
+++ controller/register_guest.php
@@ -17,7 +17,11 @@
         $password = htmlspecialchars(stripslashes($_POST['user_password']));
         $passport = $_FILES['passport']['name'];
         $passport_folder = "../passports/".$passport;
-        
+        $passport_size = $_FILES['passport']['size'];
+        $allowed_ext = array('png', 'jpg', 'jpeg', 'webp');
+        /* get current file extention */
+        $file_ext = explode('.', $passport);
+        $file_ext = strtolower(end($file_ext));
         $cur_time = Date("Y");
         $reg_number = "GUEST/".$cur_time ."/00";
         if(strlen($password) < 5){
@@ -33,50 +37,59 @@
                 $_SESSION['error'] = "User already exists!";
                 header("Location: ../guests/guest_registration.php");
             }else{
-                if(move_uploaded_file($_FILES['passport']['tmp_name'], $passport_folder)){
-                    $insert_user = $connectdb->prepare("INSERT INTO users (guest_type, gender, last_name, first_name, user_email, whatsapp, dob, country, fee, guest_password, passport, reg_number, user_type, barcode) VALUES (:guest_type, :gender, :last_name, :first_name, :user_email, :whatsapp, :dob, :country, :fee, :guest_password, :passport, :reg_number, :user_type, :barcode)");
-                    $insert_user->bindvalue("guest_type", $guest_type);
-                    $insert_user->bindvalue("last_name", $last_name);
-                    $insert_user->bindvalue("first_name", $other_name);
-                    $insert_user->bindvalue("user_email", $username);
-                    $insert_user->bindvalue("whatsapp", $phone);
-                    $insert_user->bindvalue("gender", $gender);
-                    $insert_user->bindvalue("dob", $dob);
-                    $insert_user->bindvalue("country", $country);
-                    $insert_user->bindvalue("fee", $fee);
-                    $insert_user->bindvalue("reg_number", $reg_number);
-                    $insert_user->bindvalue("guest_password", $password);
-                    $insert_user->bindvalue("user_type", $user_type);
-                    $insert_user->bindvalue("barcode", $barcode_num);
-                    $insert_user->bindvalue("passport", $passport);
-                    $insert_user->execute();
-                    // echo $reg_number;
-                    if($insert_user){
-                        $_SESSION['user'] = $username;
-                        // $get_id = PDO::lastInsertId();
-                        $mem_id = $connectdb->lastInsertId();
-                        $new_reg = $reg_number.$mem_id;
-                        $barcode = $barcode_num.$mem_id;
-                        /* update reg_num and barcode */
-                        $update_reg = $connectdb->prepare("UPDATE users SET reg_number = :reg_number, barcode = :barcode WHERE user_email = :user_email");
-                        $update_reg->bindvalue("reg_number", $new_reg);
-                        $update_reg->bindvalue("barcode", $barcode);
-                        $update_reg->bindvalue("user_email", $username);
-                        $update_reg->execute();
-                        /* update payment status */
-                        
-                        $_SESSION['reg_success'] = "Your registration was successful. Proceed to make payment for registration approval!";
-                        header("Location: ../guests/guests.php");
-                        
-                    }else{
-                        $_SESSION['error'] = "failed to register";
-                        header("Location: ../guests/guest_registration.php");
+                if(in_array($file_ext, $allowed_ext)){
+                    if($passport_size <= 300000){
+                        if(move_uploaded_file($_FILES['passport']['tmp_name'], $passport_folder)){
+                            $insert_user = $connectdb->prepare("INSERT INTO users (guest_type, gender, last_name, first_name, user_email, whatsapp, dob, country, fee, guest_password, passport, reg_number, user_type, barcode) VALUES (:guest_type, :gender, :last_name, :first_name, :user_email, :whatsapp, :dob, :country, :fee, :guest_password, :passport, :reg_number, :user_type, :barcode)");
+                            $insert_user->bindvalue("guest_type", $guest_type);
+                            $insert_user->bindvalue("last_name", $last_name);
+                            $insert_user->bindvalue("first_name", $other_name);
+                            $insert_user->bindvalue("user_email", $username);
+                            $insert_user->bindvalue("whatsapp", $phone);
+                            $insert_user->bindvalue("gender", $gender);
+                            $insert_user->bindvalue("dob", $dob);
+                            $insert_user->bindvalue("country", $country);
+                            $insert_user->bindvalue("fee", $fee);
+                            $insert_user->bindvalue("reg_number", $reg_number);
+                            $insert_user->bindvalue("guest_password", $password);
+                            $insert_user->bindvalue("user_type", $user_type);
+                            $insert_user->bindvalue("barcode", $barcode_num);
+                            $insert_user->bindvalue("passport", $passport);
+                            $insert_user->execute();
+                            // echo $reg_number;
+                            if($insert_user){
+                                $_SESSION['user'] = $username;
+                                // $get_id = PDO::lastInsertId();
+                                $mem_id = $connectdb->lastInsertId();
+                                $new_reg = $reg_number.$mem_id;
+                                $barcode = $barcode_num.$mem_id;
+                                /* update reg_num and barcode */
+                                $update_reg = $connectdb->prepare("UPDATE users SET reg_number = :reg_number, barcode = :barcode WHERE user_email = :user_email");
+                                $update_reg->bindvalue("reg_number", $new_reg);
+                                $update_reg->bindvalue("barcode", $barcode);
+                                $update_reg->bindvalue("user_email", $username);
+                                $update_reg->execute();
+                                /* update payment status */
+                                
+                                $_SESSION['reg_success'] = "Your registration was successful. Proceed to make payment for registration approval!";
+                                header("Location: ../guests/guests.php");
+                                
+                            }else{
+                                $_SESSION['error'] = "failed to register";
+                                header("Location: ../guests/guest_registration.php");
 
+                            }
+                        }else{
+                            $_SESSION['failed'] = "logo upload failed";
+                        header("Location: ../guests/guest_registration.php");
+                        }
+                    }else{
+                        $_SESSION['error'] = "Error! Image size too large! Image should not be larger than 300kb";
+                        header("Location: ../guests/guest_registration.php");
                     }
                 }else{
-                    $_SESSION['failed'] = "logo upload failed";
+                    $_SESSION['error'] = "Error! Image format not supported";
                     header("Location: ../guests/guest_registration.php");
-
                 }
             }    
         }
